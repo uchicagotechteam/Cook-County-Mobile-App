c7aa9517b7796cf147b6e6baa593561cdcdecf03
bryan22lee <bryan22lee@gmail.com>
2021-01-23 20:59:24 -0600

    reconfigure to work with snack.expo.io environment

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import { useEffect, useState, useCallback, useRef } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Button, View, Text, Image, ImageBackground, ScrollView,
-         TouchableHighlight, StyleSheet} from 'react-native';
-
+import { Button, View, Alert, Text, Image, ImageBackground, ScrollView, TouchableHighlight, StyleSheet} from 'react-native';
+import YoutubePlayer from "react-native-youtube-iframe";
+import axios from 'axios';
 
 
 var styles = StyleSheet.create({
@@ -78,11 +79,75 @@ function HomeScreen({ navigation }) {
 }
 
 function ChildScreen({ navigation }) {
+
+    // logic to maintain state of video play status
+    const [playing, setPlaying] = useState(false);
+
+    // logic to send alert when video ends
+    const onStateChange = useCallback((state) => {
+      if (state === "ended") {
+        setPlaying(false);
+        Alert.alert("video has finished playing!");
+      }
+    }, []);
+
+    // logic for play/pause button
+    const togglePlaying = useCallback(() => {
+      setPlaying((prev) => !prev);
+    }, []);
+
+    // logic to maintain state of request to youtube API
+    let [responseData, setResponseData] = useState('');
+
+    // TODO: set up env vars with expo so that we can store API KEY in .env file
+    // but we do need to see if there's a safer way to store the API key
+    // since env variables are bundled into the app's build and thus can be exposed to clients after the app is shipped
+    // more info here: https://docs.expo.io/guides/environment-variables/
+    // const API_KEY = process.env.EXPO_CCMA_YT_API_KEY;
+    const API_KEY = "hello world"; // dummy value to build
+
+    // TODO: right now the url passed into axios just gets basic stats of the channel by username
+    // read Youtube Data API docs to find what methods we need to list the channel's playlists and list videos for a playlist
+    // we need a video ID to be able to render that video in the IFrame
+    // docs here: https://developers.google.com/youtube/v3/docs/channels/list
+
+    // logic to fetch data from youtube api
+    const fetchData = useCallback(() => {
+      axios({
+        "method": "GET",
+        "url": `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername=GoogleDevelopers&key=${API_KEY}`
+        
+      })
+      .then((response) => {
+        setResponseData(response.data)
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }, [])
+
+    // useEffect function runs when function initially loads
+    // and runs again whenever there is a change to data in second argument array (fetchData)
+    // beware of infinite loops
+    useEffect(() => {
+      fetchData()
+    }, [fetchData])
+
   return (
-    // Change to ScrollView tag below
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <ScrollView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       {/* Vertical padding */}
-      <View style={{ height: 700, }} />
+      <View style={{ height: 1100, }} />
+
+      <YoutubePlayer
+        height={300}
+        play={playing}
+        videoId={"iee2TATGMyI"}
+        onChangeState={onStateChange}
+      />
+      <Button title={playing ? "pause" : "play"} onPress={togglePlaying} />
+      <Text>{JSON.stringify(responseData)}</Text>
+
       <Image
         style={styles.regLogo}
         source={require('./images/rainbow.jpg')}
@@ -115,14 +180,13 @@ function ChildScreen({ navigation }) {
             source={require('./images/up_arrow.png')}
           />
       </TouchableHighlight>
-    </View>
+    </ScrollView>
   );
 }
 
 function AdultScreen({ navigation }) {
   return (
-    // Change to ScrollView tag below
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <ScrollView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       {/* Vertical padding */}
       <View style={{ height: 150, }} />
       <Image
@@ -162,7 +226,7 @@ function AdultScreen({ navigation }) {
             source={require('./images/up_arrow.png')}
           />
       </TouchableHighlight>
-    </View>
+    </ScrollView>
   );
 }
 
--- README.md
+++ README.md
@@ -1,17 +1,58 @@
 # Cook County Mobile App - Project Rainbow
 
-A mobile app allowing parents and children affeced by COVID-19 to access educational videos and materials created by the Cook County Board.
+A mobile app allowing parents and children affected by COVID-19 to access educational videos and materials created by the Cook County Board.
 
-We are using expo at snack.expo.io to emulate on Android
+We are using expo at snack.expo.io for development and to emulate on Android.
 
-## Prerequisites
+## Youtube API
 
-Built from [TechTeam React Native Docker template project](https://github.com/uchicagotechteam/Docker-React).
+### Status
+1. When user opens the app, a hardcoded youtube video appears along with a play/pause button.
+1. When the screen loads, we make an http request to Youtube Data API that dumps the response onto the page
 
-For prerequisites, see the [wiki page](https://github.com/uchicagotechteam/Cook-County-Mobile-App/wiki/Prerequisites).
+Note that the Youtube Data API call won't work if you run it before inserting a real API key on line 33 of `App.js`
+
+Reach out to Dallon on slack/email (dasnes@uchicago.edu)/FB for any questions or to get the api key
+
+### Next steps
+`TODOs` with more details are in `App.js` file
+
+High Priority:
+1. Modify query string of http request to Youtube Data API to pull down list of playlists for that channel as well as videoIds for videos in each playlist
+1. Modify styling to match wireframe that Abbey posted in `youtube-api` slack channel
+1. Extract spaghetti code into components/variables
+1. Figure out to handle API key. Current questions are 1) how to embed API key in env variable, 2) is it even safe to put the API key in an .env file if that is exposed to client after build process and 3) should we make an org gmail acct/API key so that we stop using Rithvik's personal key?
+
+Lower Priority:
+1. Integrate expo build process with Docker
+1. Add a unit test suite
+
+### How to setup for local development
+
+We use expo for development and testing in both iOS and Android
+
+1. Clone down this repo
+1. Put in a real API key on line 33 of `App.js`
+1. Run `yarn install` to install all the required modules
+1. run `yarn ios` to run on iPhone emulator (requires Mac and iPhone emulator setup) or run `yarn android` to run on Android Emulator (requires AndroidStudio and AndroidEmulator setup)
+
+Some things to note:
+1. Whenever you want to install a new package, you have to do `expo install <package-name>` instead of `yarn add ...` or `npm install ...` so that expo can manage the cross-device deps
+1. Features such as webview (required for youtube iframe) do not work in web client, but they do work in iOS and Android
+1. If you haven't developed in XCode with an iOS simulator before, you'll need to set up a simulator
+1. Likewise you'll need to make sure you have AndroidStudio installed and set up an Android Emulator
 
 
-## Building and Running
-Run `make start` to build and run the Docker container. 
+#### Misc documentation
 
-The project will run locally in `http://localhost:3000` and will compile automatically upon changes to the src code.
+Docs for youtube iframe: https://lonelycpp.github.io/react-native-youtube-iframe/component-props
+
+Setup is based on these instructions:
+
+https://reactnative.dev/docs/environment-setup
+
+https://docs.expo.io/versions/latest/sdk/webview/
+
+https://lonelycpp.github.io/react-native-youtube-iframe/
+
+
