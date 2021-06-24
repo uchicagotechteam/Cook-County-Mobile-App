d1c9592922844a737b71a3ecf7e5f9512409c853
Vince <vincent.lagrassa@gmail.com>
2021-06-24 01:21:52 -0400

    Convert to class definition and add touch start/stop tracking
    It turns out the combination of manually setting the coordinates to
    create a looping effect and using the pagination feature of ScrollView
    was causing the component to jitter.  By tracking certain events, we can
    simulate the pagination behavior, while also setting up the component to
    handle automatic periodic scrolling.
    
    Solution uses PanHandler to fire an event when the user starts dragging the
    carousel and when they stop dragging the carousel.
    
    Had to convert the component to a class to take advantge of state -
    specifically, PanHandler was having trouble using the current version of
    the `interval` variable in its setTimeout callback.

--- components/LoopCarousel.js
+++ components/LoopCarousel.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { View, ScrollView, Text, Dimensions } from 'react-native'
+import { View, ScrollView, Text, Dimensions,
+		 Animated, PanResponder, Component
+	} from 'react-native'
 
 // Retrieve a prop value, allowing undefined values
 function getProp(props, field) {
@@ -30,65 +32,95 @@ function defaultRenderItem(item, index, width) {
 }
 
 // The class definition
-export const LoopCarousel = (props) => {
 
-	// Retrieve the basic props from the props object
-	const items = getPropRequired(props, "items");
-	const style = getProp(props, "style");
+export default class LoopCarousel extends React.Component {
+	constructor(props) {
+		super();
 
-	// Retrieve the optional props from the props object
-	const itemsPerInterval = getPropDefault(props, "itemsPerInterval", 1);
-	const renderItem       = getPropDefault(props, "renderItem",       defaultRenderItem);
+		// Retrieve the basic props from the props object
+		this.items = getPropRequired(props, "items");
+		this.style = getProp(props, "style");
 
-	// Create a ref for the ScrollView component so we can access its scrollTo method
-	const scroll_view_ref = useRef(null);
+		// Retrieve the optional props from the props object
+		this.itemsPerInterval = getPropDefault(props, "itemsPerInterval", 1);
+		this.renderItem       = getPropDefault(props, "renderItem",       defaultRenderItem);
 
-	// Compute the width of a single item in the carousel
-	// TODO: This should probably update dynamically with the dimensions of the screen
-	const itemWidth = Math.round(Dimensions.get('window').width / itemsPerInterval);
+		// Compute the width of a single item in the carousel
+		// TODO: This should probably update dynamically with the dimensions of the screen
+		this.itemWidth = Math.round(Dimensions.get('window').width / this.itemsPerInterval);
 
-	// Define a state variable to track the width of the contents
-	const [width, setWidth] = React.useState(0);
+		// The list to use as the actual data for the ScrollView element - make a copy of the
+		// elements on either side, so the user can scroll infinitely in either direction
+		// For a more intensive list we might want to use a smaller/smarter selection,
+		// but for a series of images this shouldn't noticeably affect performance
+		this.extended_items = this.items.concat(this.items).concat(this.items);
 
-	// Define a state variable to track the current item at the left side of the screen
-	const [interval, setInterval] = React.useState(items.length);
+		// Set the upper and lower bounds for the scroll, and compute the distance between them
+		// Used to keep the view on the center copy of the items
+		this.coord_lower_bound = this.items.length * this.itemWidth;
+		this.coord_upper_bound = this.items.length * this.itemWidth * 2;
+		this.coord_width       = this.coord_upper_bound - this.coord_lower_bound;
 
-	// The list to use as the actual data for the ScrollView element - make a copy of the
-	// elements on either side, so the user can scroll infinitely in either direction
-	// For a more intensive list we might want to use a smaller/smarter selection,
-	// but for a series of images this shouldn't noticeably affect performance
-	const extended_items = items.concat(items).concat(items);
+		// Create a ref for the ScrollView component so we can access its scrollTo method
+		this.scroll_view_ref = React.createRef();
 
-	// Set the upper and lower bounds for the scroll, and compute the distance between them
-	// Used to keep the view on the center copy of the items
-	const coord_lower_bound = items.length * itemWidth;
-	const coord_upper_bound = items.length * itemWidth * 2;
-	const coord_width       = coord_upper_bound - coord_lower_bound;
+		// Track when the user starts and stops interacting with the carousel
+		this.panResponder = PanResponder.create({
+			onMoveShouldSetPanResponder: () => true,
+
+			// When the user first grabs the carousel, cancel any existing timeout
+			onPanResponderGrant: () => {
+				if (this.state.curr_timeout !== null) {
+					clearTimeout(this.state.curr_timeout);
+					this.setState({ curr_timeout: null });
+				}
+			},
+
+			// When the user lets go of the carousel, set a timeout to snap to an interval
+			onPanResponderRelease: () => {
+				this.state.curr_timeout = setTimeout(() => this.snap_to_interval(), 1000);
+			},
+		});
+
+		// Store the variables we expect might change in the state
+		this.state = {
+			width: 0,
+			interval: this.items.length,
+			curr_timeout: null,
+		};
+	}
 
 	// Init function to run any time the dimensions of the content change
-	const init = (width) => {
-		setWidth(width);
+	init(width) {
+		this.setState({ width });
+
+		// Start at the first element of the center copy
+		this.scrollTo({ x: this.items.length * this.itemWidth, animated: false });
 	}
 
 	// Get the index in extended_items that the LoopCarousel currently has on the left edge
 	// of the screen
-	const getAbsoluteInterval = (offset) => {
-		for (let i = 1; i <= extended_items.length; i++) {
-			if (offset < itemWidth * i) {
+	getAbsoluteInterval(offset) {
+		for (let i = 1; i <= this.extended_items.length; i++) {
+			if (offset < this.itemWidth * i) {
 				return i;
 			}
 		}
-		return extended_items.length;
+		return this.extended_items.length;
 	}
 
 	// Get the index in items that the LoopCarousel currently has on the left edge of the screen
-	const getInterval = (offset) => {
-		var interval = getAbsoluteInterval(offset);
-		return items.length + ((interval - 1) % items.length);
+	getInterval(offset) {
+		var interval = this.getAbsoluteInterval(offset);
+		return this.items.length + ((interval - 1) % this.items.length);
+	}
+
+	setInterval(val) {
+		this.setState({ interval: val });
 	}
 
 	// On the scroll event, check if the offset is going past the boundaries, and adjust it if so
-	const onScroll = (nativeEvent) => {
+	onScroll(nativeEvent) {
 
 		// WARNING: contentSize not available on Android
 		const { layoutMeasurement, contentOffset, contentSize } = nativeEvent;
@@ -98,40 +130,48 @@ export const LoopCarousel = (props) => {
 
 		// If above the upper bound, compute the adjustment needed to loop back around
 		// to the lower bound, and vice versa
-		while (contentOffset.x + adjustment > coord_upper_bound) {
-			adjustment -= coord_width;
+		while (contentOffset.x + adjustment > this.coord_upper_bound) {
+			adjustment -= this.coord_width;
 		}
-		while (contentOffset.x + adjustment < coord_lower_bound) {
-			adjustment += coord_width;
+		while (contentOffset.x + adjustment < this.coord_lower_bound) {
+			adjustment += this.coord_width;
 		}
 
 		// If an adjustment is needed to bring the window back into the proper range, apply it
 		if (adjustment != 0) {
-			scroll_view_ref.current.scrollTo({x: contentOffset.x + adjustment, animated: false});
+			this.scrollTo({ x: contentOffset.x + adjustment, animated: false });
 		}
 
 		// Set the interval value
-		setInterval(getInterval(contentOffset.x));
+		this.setInterval(this.getInterval(contentOffset.x));
+	}
+
+	scrollTo(...args) {
+		this.scroll_view_ref.current.scrollTo(...args);
+	}
+
+	snap_to_interval() {
+		this.scrollTo({ x: this.state.interval * this.itemWidth, animated: true });
 	}
 
 	// Return a ScrollView element, whose entries consist of the items array with renderItem
 	// mapped over each item
-	return (
-		<View>
-			<ScrollView ref={scroll_view_ref}
-				horizontal={true}
-				contentContainerStyle={{ width: `${itemWidth * extended_items.length}px` }}
-				showsHorizontalScrollIndicator={false}
-		        onContentSizeChange={(w, h) => init(w)}
-		        scrollEventThrottle={16}
-		        pagingEnabled
-		        decelerationRate="fast"
-		        onScroll={({nativeEvent}) => onScroll(nativeEvent)}
-			>
-				{extended_items.map( (item, idx) => renderItem(item, idx, itemWidth) )}
-			</ScrollView>
-		</View>
-	)
+	render() {
+		const itemWidth = this.itemWidth
+		return (
+			<Animated.View {...this.panResponder.panHandlers} >
+				<ScrollView ref={this.scroll_view_ref}
+					horizontal={true}
+					contentContainerStyle={{ width: `${itemWidth * this.extended_items.length}px` }}
+					showsHorizontalScrollIndicator={false}
+					onContentSizeChange={(w, h) => this.init(w)}
+					scrollEventThrottle={64}
+					decelerationRate="fast"
+					onScroll={({nativeEvent}) => this.onScroll(nativeEvent)}
+				>
+					{this.extended_items.map( (item, idx) => this.renderItem(item, idx, itemWidth) )}
+				</ScrollView>
+			</Animated.View>
+		)
+	}
 }
-
-export default LoopCarousel;
