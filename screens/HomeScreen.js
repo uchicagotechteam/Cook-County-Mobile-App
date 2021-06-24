c05b7fe64568044ffd2928985fd22a867bae186c
Vince <vincent.lagrassa@gmail.com>
2021-06-24 16:51:52 -0400

    Clean up / update home screen code
    Import search & channel logic from AdultScreen.js
    
    Better organize code for sections of home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,40 +3,79 @@ import { useEffect, useState, useCallback, useRef } from "react";
 import { TouchableOpacity, View, Text, Image, ImageBackground, StyleSheet } from 'react-native';
 import { styles } from '../scripts/constants.js'
 import RoundedButton from '../components/RoundedButton.js'
+
+// The components used in the Homescreen
 import SponsorBanner from '../components/SponsorBanner.js'
+import { SearchBar } from 'react-native-elements';
+import ChannelCollection from "../components/ChannelCollection.js";
 
 function HomeScreen({ navigation }) {
   // const source = {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf',cache:true};
 
+  // Logic to maintain state of search text
+  const [searchText, setSearchText] = useState("");
+  const [dateInfo, setDateInfo] = useState({
+    dateRestriction : "Anytime",
+    afterDate : null,
+    beforeDate : null
+  });
+
+  // Function to update the search results
+  const updateSearch = useCallback((search) => {
+    setSearchText(search);
+  }, []);
+
+  // Array of objects containing the information needed to populate a channel (TODO: figure out if this is okay to hardcode)
+  const channels = [
+    { channelTitle : "Test Channel 1",
+      channelImage : "music",
+      playlistId : "PLWgiRpr4E_tV2_sL7r-6eGxVDN8EJBkkZ",
+    },
+    { channelTitle : "Test Channel 2",
+      channelImage : "music",
+      playlistId : "PLWgiRpr4E_tV2_sL7r-6eGxVDN8EJBkkZ",
+    },
+    { channelTitle : "Test Channel 3",
+      channelImage : "music",
+      playlistId : "PLWgiRpr4E_tV2_sL7r-6eGxVDN8EJBkkZ",
+    },
+    { channelTitle : "Test Channel 4",
+      channelImage : "music",
+      playlistId : "PLWgiRpr4E_tV2_sL7r-6eGxVDN8EJBkkZ",
+    },
+  ];
+
   return (
     <View style={styles.centerColumn}>
-      { /* <ImageBackground style={styles.bgLogo} source={require('../images/simple_rainbow.png')}> */ }
+
+      {/* TODO: Featured videos banner */}
+
+      {/* The banner showing the logo for each sponsor */}
       <SponsorBanner
         image_ids={[ "19Y4tCXEbft3isAWAT-4l34t8fRiZzpWE" ]}
         shuffle={true}
       />
-      <Image
-        style={styles.mainLogo}
-        source={require('../images/PR_logo_name.png')}
-      />
-      { /* </ImageBackground> */ }
-      <Text style={styles.titleText}>
-        Welcome to Project Rainbow! {"\n"} I am a:
-      </Text>
-      <View style={{margin:20}}>
-      <RoundedButton
-        onPress={() => navigation.navigate('Adult Page')}
-        buttonStyle={styles.mainButtonStyle}
-        textStyle={styles.mainButtonText}
-        text={"Teacher"}
-      />
+
+      {/* The shelf view for the Rainbow Channels */}
+      <View style={{width: "100%"}}>
+
+        {/* The search bar for the channels */}
+        <SearchBar
+          placeholder="Search title or date"
+          onChangeText={updateSearch}
+          value={searchText}
+          platform={"android"}
+          round={true}
+        />
+
+        {/* The list of channels themselves */}
+        <ChannelCollection
+          channels={channels}
+          searchText={searchText}
+          dateInfo={dateInfo}
+          isAdult={true}
+        />
       </View>
-      <RoundedButton
-        onPress={() => navigation.navigate('Child Page')}
-        buttonStyle={styles.mainButtonStyle}
-        textStyle={styles.mainButtonText}
-        text={"Student"}
-      />
     </View>
   );
 }
