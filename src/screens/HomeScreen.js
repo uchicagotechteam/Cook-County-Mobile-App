181cff167a72847d6b7c2343899b5e8dba7462a9
Vince <vincent.lagrassa@gmail.com>
2021-08-05 10:40:12 -0500

    Clean up homescreen code a bit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -89,6 +89,9 @@ function HomeScreen({ navigation }) {
   //   },
   // ];
 
+  // Get the dimensions of the screen
+  const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('screen')
+
   // Set the height of the header when expanded and when collapsed
   const HEADER_EXP_HEIGHT = 180;
   const HEADER_COL_HEIGHT = 50;
@@ -122,106 +125,11 @@ function HomeScreen({ navigation }) {
     extrapolate: 'clamp'
   });
 
-  // Get the dimensions of the screen
-  const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('screen')
-
-  // return (
-  //   <View style={{flex: 1}}>
-  //     <Animated.View style={{
-  //       height: headerHeight, width: SCREEN_WIDTH,
-  //       position: 'absolute', top: 0, left: 0
-  //     }}/>
-  //       <ScrollView
-  //         contentContainerStyle={{ padding: 16, paddingTop: HEADER_EXP_HEIGHT }}
-  //         onScroll={Animated.event(
-  //           [{ nativeEvent: { contentOffset: { y: scroll_y } } }],
-  //           { useNativeDriver: false }
-  //         )}
-  //         scrollEventThrottle={16}
-  //       >
-  //         <Text style={{fontSize: 24, marginVertical: 16}}>This is Title</Text>
-  //         <Text>
-  //           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id purus nec sapien semper bibendum vel commodo nulla. Integer vel fermentum mi. Nullam rutrum elit et nisi scelerisque, at dictum sem sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ornare dui in nunc pretium, ut elementum est porta. Proin et mollis tortor, vel pulvinar mauris. Nam fringilla, erat sit amet lobortis euismod, purus magna faucibus erat, a bibendum dui neque sit amet magna. Aenean ultricies, ipsum ac tincidunt mattis, augue libero vestibulum urna, et dignissim urna mauris ac tellus. Suspendisse luctus risus sed accumsan porttitor. Cras pretium scelerisque nibh vel aliquet.
-  //           In hac habitasse platea dictumst. Quisque tempor ut nibh quis elementum. Mauris dignissim purus ut luctus mattis. Nulla facilisi. Praesent convallis ornare eros, a aliquam magna facilisis a. Vivamus a ligula velit. Pellentesque bibendum nunc vel urna rutrum rhoncus. Aliquam blandit, purus quis suscipit euismod, ipsum arcu egestas nunc, ut lacinia massa dolor sed leo. Donec eget tincidunt odio. Nunc convallis sem dui, nec ullamcorper tellus sodales non. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris cursus convallis urna, id tempus magna pretium nec.
-  //           Aliquam non lorem leo. Curabitur molestie non sapien ac facilisis. Donec eleifend porttitor viverra. Proin ut porttitor erat. Vivamus dapibus nunc eget ipsum euismod, facilisis porta enim vulputate. Aenean et purus sit amet mi tincidunt lacinia. Aliquam erat volutpat. Integer a purus ullamcorper dui auctor fermentum. Suspendisse porta, quam at iaculis gravida, mauris dui pretium erat, quis volutpat purus risus ac nisl. Integer quam nibh, lacinia sit amet risus ultrices, bibendum faucibus arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut porta et neque a varius. Nunc sed facilisis mi, quis molestie felis. Nunc vel massa at quam pretium posuere ac sit amet quam. Proin ullamcorper gravida ante sed fringilla. Nullam maximus dui quis velit lobortis, ac molestie mauris pulvinar.
-  //           Maecenas scelerisque laoreet purus ac sodales. In fermentum quam et lectus ultricies, vel pulvinar magna molestie. Integer porttitor velit nulla. Vestibulum dapibus consectetur enim ac placerat. Etiam aliquet, libero et feugiat commodo, leo quam accumsan magna, in vulputate justo ex eu quam. Proin in diam non eros facilisis sodales sed sit amet odio. Donec in viverra est. Curabitur lacus urna, tincidunt ac augue vel, molestie efficitur dui.
-  //           Duis cursus ante id placerat tincidunt. In vel orci velit. Donec iaculis ac elit ut interdum. Fusce non tristique nisi, vitae laoreet augue. Praesent semper, ex sit amet tincidunt tristique, nunc massa condimentum libero, vitae interdum ex lorem ac augue. Maecenas porta non diam in elementum. Quisque mattis efficitur libero, in pulvinar tellus posuere non. Nunc turpis mi, eleifend at felis non, placerat volutpat arcu. Quisque bibendum in velit a pharetra. Nulla facilisi. Fusce convallis felis massa.
-  //         </Text>
-  //       </ScrollView>
-  //   </View>
-  // );
-
-
-
-
-  // return (
-  //   <View style={[ styles.centerColumn ]}>
-
-  //     {/* The collapsing header component */}
-  //     <View style={{ height: 300 }}>
-
-  //       {/* TODO: Featured videos banner */}
-
-  //       {/* The banner showing the logo for each sponsor */}
-  //       <SponsorBanner
-  //         // image_ids={[ "19Y4tCXEbft3isAWAT-4l34t8fRiZzpWE" ]}
-  //         image_ids={[
-  //           "19Y4tCXEbft3isAWAT-4l34t8fRiZzpWE",
-  //           "19Y4tCXEbft3isAWAT-4l34t8fRiZzpWE",
-  //           "19Y4tCXEbft3isAWAT-4l34t8fRiZzpWE",
-  //           "19Y4tCXEbft3isAWAT-4l34t8fRiZzpWE",
-  //           "19Y4tCXEbft3isAWAT-4l34t8fRiZzpWE",
-  //           "19Y4tCXEbft3isAWAT-4l34t8fRiZzpWE",
-  //           "19Y4tCXEbft3isAWAT-4l34t8fRiZzpWE",
-  //           "19Y4tCXEbft3isAWAT-4l34t8fRiZzpWE",
-  //         ]}
-  //         navigation={navigation}
-  //         shuffle={true}
-  //         style={{ marginVertical: 40 }}
-  //       />
-
-  //     </View>
-
-  //     { The shelf view for the Rainbow Channels }
-  //     <ScrollView
-  //       style={{ width: "100%", height: 300 }}
-  //       contentContainerStyle={{  }}
-  //       onScroll={Animated.event(
-  //         [{ nativeEvent: { contentOffset: { y: scroll_y } } }],
-  //         { useNativeDriver: false }
-  //       )}
-  //       scrollEventThrottle={16}
-  //       stickyHeaderIndices={[0]}
-  //     >
-
-  //       {/* The search bar for the channels */}
-  //       <SearchBar
-  //         placeholder="Search title or date"
-  //         onChangeText={updateSearch}
-  //         value={searchText}
-  //         platform={"android"}
-  //         round={true}
-  //       />
-
-  //       {/* The list of channels themselves */}
-  //       <ChannelCollection
-  //         navigation={navigation}
-  //         channels={channels}
-  //         searchText={searchText}
-  //         dateInfo={dateInfo}
-  //         isAdult={true}
-  //       />
-  //     </ScrollView>
-  //   </View>
-  // );
-
-
-
-
-
+  // Create state variables to track and control scrolling
   let [isScrolling,   setScrolling]     = useState(false);
   let [scrollEnabled, setScrollEnabled] = useState(true);
 
+  // Callbacks to temporarily disable vertical scrolling on the homepage
   const onGrab = () => {
     if (!isScrolling) setScrollEnabled(false);
   }
@@ -229,11 +137,11 @@ function HomeScreen({ navigation }) {
     setScrollEnabled(true);
   }
 
+  // Callbacks to track whether the screen is actvely scrolling
   const startScroll = () => {
     setScrollEnabled(true);
     setScrolling(true);
   }
-
   const endScroll = () => {
     setScrolling(false);
   }
