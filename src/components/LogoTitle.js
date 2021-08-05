970eeac7d904853273508211e7ef893b8276330a
Vince <vincent.lagrassa@gmail.com>
2021-08-05 18:30:05 -0500

    Put logo and title vertical

--- src/components/LogoTitle.js
+++ src/components/LogoTitle.js
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Image } from 'react-native';
 import AdjustableText from "../components/AdjustableText.js";
+import LogoImage from '../components/LogoImage.js';
+
+// Import functions to retrieve props
+import { getProp, getPropRequired, getPropDefault } from "../scripts/GetProps.js";
+
 
 //mini org logo and org name
 //line 20 {props.channel.channelTitle}
@@ -9,27 +14,46 @@ import AdjustableText from "../components/AdjustableText.js";
 
 
 export default function LogoTitle(props) {
+
+  // Retrieve required channel prop
+  const channel = getPropRequired(props, "channel", "LogoTitle");
+
+  // Retrieve information from channel
+  const title = channel.channelTitle;
+  const image_id = channel.image_id;
+
+  const style = getPropDefault(props, "style", {});
+
   return (
-    <View style={styles.container}>
-    <View style={styles.row}>
-    <View style={styles.column1}>
-      <Image
-        style={styles.logo}
-        source={{uri: "https://drive.google.com/thumbnail?id=" + props.channel.image_id }}
+    <View style={[{alignItems: "center", width: "100%"}, style]}>
+      <LogoImage imageId={image_id} width={175} clickable={false} />
+      <AdjustableText
+        text=<Text>{title}</Text>
+        fontSize={30} maxHeight={60}
       />
-      </View>
-      <View style={styles.column2}>
-          <AdjustableText
-            fontSize={30}
-            text=<Text> {props.channel.channelTitle}
-                  </Text>
-            style = {styles.column2}
-            maxHeight={60}
-          />
-      </View>
-    </View>
     </View>
   );
+
+  // return (
+  //   <View style={styles.container}>
+  //     <View style={styles.row}>
+  //       <View style={styles.column1}>
+  //         <Image
+  //           style={styles.logo}
+  //           source={{uri: "https://drive.google.com/thumbnail?id=" + image_id }}
+  //         />
+  //       </View>
+  //       <View style={styles.column2}>
+  //           <AdjustableText
+  //             fontSize={30}
+  //             text=<Text>{title}</Text>
+  //             style={styles.column2}
+  //             maxHeight={60}
+  //           />
+  //       </View>
+  //     </View>
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
