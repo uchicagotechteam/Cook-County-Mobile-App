5ae9afdeaf69cc2ed071846fc70db870c7542606
Vince <vincent.lagrassa@gmail.com>
2021-08-10 09:35:22 -0500

    Uncomment getLaunchOptions

--- android/app/src/main/java/com/projectrainbow/MainActivity.java
+++ android/app/src/main/java/com/projectrainbow/MainActivity.java
@@ -30,12 +30,12 @@ public class MainActivity extends ReactActivity {
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
         return new ReactActivityDelegate(this, getMainComponentName()) {
-//            @Override
-//            protected Bundle getLaunchOptions() {
-//                Bundle launchOptions = new Bundle();
-//                launchOptions.putString("buildType", BuildConfig.BUILD_TYPE);
-//                return launchOptions;
-//            }
+            @Override
+            protected Bundle getLaunchOptions() {
+                Bundle launchOptions = new Bundle();
+                launchOptions.putString("buildType", BuildConfig.BUILD_TYPE);
+                return launchOptions;
+            }
 
             @Override
             protected ReactRootView createRootView() {
