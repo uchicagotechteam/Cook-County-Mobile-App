9dc238c0334b72495c26f3200c36ace26d41ee2c
ellienewman <ellienewman@Ellies-MacBook-Air.local>
2021-08-10 21:42:40 -0400

    added playlists

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -47,7 +47,7 @@ function HomeScreen({ navigation }) {
   }, []);
 
     // // Array of objects containing the information needed to populate a channel (TODO: figure out if this is okay to hardcode)
-  const channelArray = [
+ const channelArray = [
     { channelTitle : "Brookfield Zoo",
        channelImage : require('../assets/images/BrookfieldZoo.png'),
        playlistId : "PL8GKxgb3LyNcB01ujLEDS1NH27YqYsOmD",
@@ -64,6 +64,22 @@ function HomeScreen({ navigation }) {
        channelImage : require('../assets/images/MuseumofMexicanArt.png'),
        playlistId : "PL8GKxgb3LyNfBnnwR6pc6JHZUIFLlg-rK",
     },
+    {  channelTitle : "Project Rainbow",
+       channelImage : require('../assets/images/PR_logo_name.png'),  
+       playlistId : "PL8GKxgb3LyNez6eQhoJCu5sJX5xDCkMQt",
+    },
+    {  channelTitle : "Negaunee Music Institute at the Chicago Symphony Orchestra",
+       channelImage : require('../assets/images/ChicagoSymphony.png'),
+       playlistId : "PL8GKxgb3LyNfmdA1XmlTf24ZWj7Hwe52S",
+    },
+    {  channelTitle : "Chicago Botanic Garden",
+       channelImage : require('../assets/images/BotanicGarden.png'),
+       playlistId : "PL8GKxgb3LyNcBU5mOtyhyhpWlXkP-iHxI",
+    },
+    {  channelTitle : "Museum of Contemporary Art Chicago",
+       channelImage : require('../assets/images/mca.png'),  
+       playlistId : "PL8GKxgb3LyNfQ8wz4GaXKXc8qhn0qthx_",
+    }
   ];
 
   const image_ids = [
@@ -71,6 +87,10 @@ function HomeScreen({ navigation }) {
     require('../assets/images/ChildrensMuseum.png'),
     require('../assets/images/ForestPreserves.png'),
     require('../assets/images/MuseumofMexicanArt.png'),
+    require('../assets/images/PR_logo_name.png'),
+    require('../assets/images/ChicagoSymphony.png'),
+    require('../assets/images/BotanicGarden.png'),
+    require('../assets/images/mca.png')
   ];
   
   // Channel id for the CCB user's channel 
